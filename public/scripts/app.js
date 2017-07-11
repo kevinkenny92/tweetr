a84aea7b3ec8e6e9a6a0ddf9740213b1d2d12b44
Kevin <kevinkenny92@gmail.com>
2017-07-11 22:25:22 +0000

    created tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(document).ready(function() {
 
+  let tweetIcons = $('.new-tweet__icons');
+
+  $('.tweet').hover(function() {
+    $(this).css('opacity', '1');
+    $(this).find(tweetIcons).css('display', 'initial');
+  }, function() {
+    $(this).css('opacity', '0.7');
+    $(this).find(tweetIcons).css('display', 'none');
+  });
+});
