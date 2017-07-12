c197a919d5c7088b97d77343f482b69f182c6607
Kevin <kevinkenny92@gmail.com>
2017-07-12 00:12:43 +0000

    Completed W3Day2Stuff

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,101 @@
-$(document).ready(function() {
 
-  let tweetIcons = $('.new-tweet__icons');
 
-  $('.tweet').hover(function() {
-    $(this).css('opacity', '1');
-    $(this).find(tweetIcons).css('display', 'initial');
-  }, function() {
-    $(this).css('opacity', '0.7');
-    $(this).find(tweetIcons).css('display', 'none');
-  });
-});
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
+
+/*
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
+//given in exercise
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+//Modifications for Last Part of Week3day2
+
+
+function renderTweets(tweets) {
+  // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  for (var tweetInfo of tweets) {
+    var $tweet = createTweetElement(tweetInfo);
+    console.log($tweet);
+    $('.new-tweet__container').prepend($tweet);
+  };
+};
+
+function createTweetElement(tweet) {
+  let output = '';
+
+  output += `<article class="tweet">
+            <header>
+              <img src="${tweet.user.avatars.small}"/>
+              <h2>${tweet.user.name}</h2>
+              <span>${tweet.user.handle}</span>
+            </header>
+            <p>
+              ${tweet.content.text}
+            </p>
+            <footer>
+              <div class="new-tweet__time-stamp">${tweet.created_at}</div>
+              <div class="new-tweet__icons">
+                <i class="fa fa-flag" aria-hidden="true"></i>
+                <i class="fa fa-retweet" aria-hidden="true"></i>
+                <i class="fa fa-heart" aria-hidden="true"></i>
+              </div>
+            </footer>
+          </article>`
+
+  return output
+}
+//Repeat again to hover over new files.
---
+++ public/scripts/hover.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  renderTweets(data);
+
+  // hover effect
+  let tweetIcons = $('.new-tweet__icons');
+
+  $('.tweet').hover(function() {
+    $(this).css('opacity', '1');
+    $(this).find(tweetIcons).css('display', 'initial');
+  }, function() {
+    $(this).css('opacity', '0.7');
+    $(this).find(tweetIcons).css('display', 'none');
+  });
+});
